252d428791a5afb67090e080efca4af79521bd2d
m-krisnandi <mkrisnandi08@gmail.com>
2022-07-03 08:09:48 +0700

    finish CRUD Category

--- server-staycation/routes/admin.js
+++ server-staycation/routes/admin.js
@@ -1,10 +1,22 @@
 const router = require('express').Router();
 const adminController = require('../controllers/adminController');
 
+// endpoint dashboard
 router.get('/dashboard', adminController.viewDashboard);
+
+// endpoint category
 router.get('/category', adminController.viewCategory);
+router.post('/category', adminController.addCategory);
+router.put('/category', adminController.editCategory);
+router.delete('/category/:id', adminController.deleteCategory);
+
+// endpoint bank
 router.get('/bank', adminController.viewBank);
+
+// endpoint item
 router.get('/item', adminController.viewItem);
+
+// endpoint booking
 router.get('/booking', adminController.viewBooking);
 
 module.exports = router;
